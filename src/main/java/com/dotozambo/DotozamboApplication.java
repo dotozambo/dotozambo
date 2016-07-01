416f9bd7afb671346951065bc4ea23f991321c15
NAVER <NAVER@NAVERui-MacBook-Pro-3.local>
2016-07-01 14:14:26 +0900

    First Source Code commit

---
+++ src/main/java/com/dotozambo/DotozamboApplication.java
@@ -0,0 +1,21 @@
+package com.dotozambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DotozamboApplication {
+
+	@RequestMapping("/")
+	public String index() {
+		return "helloworld!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DotozamboApplication.class, args);
+	}
+}
